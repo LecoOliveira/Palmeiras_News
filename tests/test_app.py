f162173587d234cfed6d4bdc49ca17ba274a46f4
LecoOliveira <lecoverde10@gmail.com>
2023-05-14 22:47:21 -0300

    Corrigindo testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,17 @@
 from app.functions import *
 import re
 
+
 my_regex = re.compile('../..')
+
+
 def test_data_jogo_retorna_uma_data():
     assert my_regex.match(data_jogo())
+
 
 def test_texto_msg_nao_deve_retornar_None():
     assert texto_msg() is not None
 
+
 def test_texto_msg_deve_retornar_algum_texto():
-    assert texto_msg() is str
+    assert type(texto_msg()) is str
