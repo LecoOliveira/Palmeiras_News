1628e0f37505468d81e873f75604c5237dc30e69
LecoOliveira <lecoverde10@gmail.com>
2023-05-21 01:45:33 -0300

    Refatorando: Colocando todas as funções em um único módulo.

--- app/app.py
+++ app/app.py
@@ -1,40 +1,14 @@
 import os
 from datetime import datetime
 
-from dotenv import load_dotenv
-from functions import data_jogo, texto_msg
-from twilio.rest import Client
-
-# Credenciais da conta Twilio, armazenadas em uma variável de ambiente.
-# Para mais informações: https://www.twilio.com/docs/usage/secure-credentials
-#                        https://www.twilio.com/blog/environment-variables-python
-load_dotenv('/home/alex/Documentos/Estudos/Palmeiras_News/app/twilio.env')
-ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
-AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
-PHONE_NUMBER = os.environ['TWILIO_PHONE_NUMBER']
-DESTINY_PHONE_NUMBER = os.environ['TWILIO_DESTINY_PHONE_NUMBER']
-CLIENT = Client(ACCOUNT_SID, AUTH_TOKEN)
-
+from functions import *
 
 # Coleta a data do sistema ja fomatada.
 data_hoje = datetime.today().strftime('%d/%m')
-
-
-def enviar_msg() -> str:
-    """Envia a mensagem para o destino definido em 'destiny_phone_number'
-
-    Returns:
-        str: Mensagem de confirmação ou de erro.
-    """
-
-    if texto_msg() != 'Não há relatos sobre este jogo.':
-        return CLIENT.messages.create(
-            body=texto_msg(), from_=PHONE_NUMBER, to=DESTINY_PHONE_NUMBER
-        )
 
 
 # if data_hoje == data_jogo() or (int(data_hoje[:2]) + 1) == int(
 #     data_jogo()[:2]
 # ):
 #     enviar_msg()
-print(texto_msg())
+print(data_jogo())
--- app/functions.py
+++ app/functions.py
@@ -1,7 +1,9 @@
 from re import compile
 
 from bs4 import BeautifulSoup as bs
+from dotenv import load_dotenv
 from requests import get
+from twilio.rest import Client
 
 URL_PRINCIPAL = 'https://www.palmeiras.com.br/home/'
 
@@ -65,4 +67,26 @@ def texto_msg() -> str:
             .find('p')
             .get_text('\n')
             .replace('\n', '\n\n')
+        )
+
+
+def enviar_msg() -> str:
+    """Envia a mensagem para o destino definido em 'destiny_phone_number'
+
+    Returns:
+        str: Mensagem de confirmação ou de erro.
+    """
+    # Credenciais da conta Twilio, armazenadas em uma variável de ambiente.
+    # Para mais informações: https://www.twilio.com/docs/usage/secure-credentials
+    #                        https://www.twilio.com/blog/environment-variables-python
+    load_dotenv('/home/alex/Documentos/Estudos/Palmeiras_News/app/twilio.env')
+    ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+    AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+    PHONE_NUMBER = os.environ['TWILIO_PHONE_NUMBER']
+    DESTINY_PHONE_NUMBER = os.environ['TWILIO_DESTINY_PHONE_NUMBER']
+    CLIENT = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+    if texto_msg() != 'Não há relatos sobre este jogo.':
+        return CLIENT.messages.create(
+            body=texto_msg(), from_=PHONE_NUMBER, to=DESTINY_PHONE_NUMBER
         )
