81a89cead673d36c5e1cf189f6f2377d6d02cf66
alex <lecoverde10@gmail.com>
2023-06-06 23:51:24 -0300

    Adicionando mais testes, e aumentando cobertura.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,10 @@
 import re
 
-from app.tasks.texto import *
+from rocketry.conds import false, true
+
+from app.arguments import *
+from app.conditions import data_igual
+from app.tasks.texto import texto_msg
 
 my_regex = re.compile('../..')
 link = link_jogo()
@@ -16,3 +20,20 @@ def test_texto_msg_nao_deve_retornar_None():
 
 def test_texto_msg_deve_retornar_algum_texto():
     assert type(texto_msg(link)) is str
+
+
+def test_data_igual_deve_retornar_false_ou_true():
+    cond = data_igual()
+    assert cond.observe() == True or cond.observe() == False
+
+
+def test_data_igual_deve_retornar_false():
+    cond = data_igual()
+    if data_hoje() != data_jogo():
+        assert cond.observe() == False
+
+
+def test_data_igual_deve_retornar_true():
+    cond = data_igual()
+    if data_hoje() == data_jogo():
+        assert cond.observe() == True
