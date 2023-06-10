62229a84119fad54bad056c20da928142996e09a
LecoOliveira <lecoverde10@gmail.com>
2023-06-10 16:15:01 -0300

    Criando teste da função envia_msg

--- tests/test_envio.py
+++ tests/test_envio.py
@@ -5,9 +5,10 @@ from app.tasks.envia import enviar_msg
 
 @mock.patch('app.tasks.envia.CLIENT.messages.create')
 def test_enviar_msg_esta_funcionando(create_message_mock):
-    message = 'oi'
     expected_sid = 'SM87105da94bff44b999e4e6eb90d8eb6a'
     create_message_mock.return_value.sid = expected_sid
-    sid = enviar_msg(message)
+    message = create_message_mock
+    sid = enviar_msg()
 
     assert create_message_mock.called is True
+    assert sid == expected_sid
