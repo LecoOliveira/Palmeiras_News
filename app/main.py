46ee88a20c0b7121c03bbf74e564fd77d9c3a400
LecoOliveira <lecoverde10@gmail.com>
2023-06-13 01:41:46 -0300

    Mudando as configs de lugar

--- app/main.py
+++ app/main.py
@@ -5,7 +5,13 @@ from rocketry.args import Config
 
 from app.tasks import envia, formata, texto
 
-app = Rocketry()
+app = Rocketry(
+    config={
+        'silence_task_prerun': False,
+        'silence_task_logging': False,
+        'silence_cond_check': False,
+    },
+)
 
 app.include_grouper(texto.group)
 app.include_grouper(envia.group)
@@ -16,14 +22,3 @@ handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
 task_logger = logging.getLogger('rocketry.task')
 task_logger.addHandler(handler)
-
-
-@app.setup()
-def set_config(config=Config()):
-    config.silence_task_prerun = False
-    config.silence_task_logging = False
-    config.silence_cond_check = False
-
-
-# if __name__ == '__main__':
-#     app.run()
