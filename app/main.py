8f53fae7087b82fe257777078ef8667eab2f4404
LecoOliveira <lecoverde10@gmail.com>
2023-06-25 13:33:43 -0300

    Implementando configurações de logging

--- app/main.py
+++ app/main.py
@@ -1,33 +1,13 @@
 import logging
+import logging.config
 
-from redbird.logging import RepoHandler
-from redbird.repos import CSVFileRepo
 from rocketry import Rocketry
-from rocketry.args import Config
-from rocketry.log import MinimalRecord
 
 from app.tasks import envia, formata, texto
 
-repo = CSVFileRepo(filename='app/task.csv', model=MinimalRecord)
-
-task_logger = logging.getLogger('rocketry.task')
-handler = RepoHandler(repo=repo)
-task_logger.addHandler(handler)
-
-app = Rocketry(
-    config={
-        'silence_task_prerun': False,
-        'silence_task_logging': False,
-        'silence_cond_check': False,
-    },
-)
+app = Rocketry()
+logging.config.fileConfig('app/config/logging.conf')
 
 app.include_grouper(texto.group)
 app.include_grouper(envia.group)
 app.include_grouper(formata.group)
-
-
-# handler = logging.StreamHandler()
-# handler.setLevel(logging.DEBUG)
-# task_logger = logging.getLogger('rocketry.task')
-# task_logger.addHandler(handler)
